993e3d0360f5fa384403bccee1c476bdab634ff7
noamtamir <noam.tamir@gmail.com>
2023-09-12 23:04:43 +0200

    remove tonejs

--- player/models.py
+++ player/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from dataclasses import dataclass
 
+BASE_URL = 'http://localhost:3000/static'
 TRACK_LINKS = {
-  'טל אנגרט  -   מאפייני הקיוםBeLateral .mp3': 'https://docs.google.com/uc?export=open&id=1wh6IUB5rCcGsEWPfanNLWVOKVMhbb9TM',
-  'טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3': 'https://docs.google.com/uc?export=open&id=1X75PDxCexZW1ikXSVd1I3PS5hmJhElBQ',
-  'אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3': 'https://docs.google.com/uc?export=open&id=1zoeXuHzsyTe5ZIscGfuGi9wqkC5SP8Sv'
+  'טל אנגרט  -   מאפייני הקיוםBeLateral .mp3': f'{BASE_URL}/טל אנגרט  -   מאפייני הקיוםBeLateral .mp3',
+  'טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3': f'{BASE_URL}/טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3',
+  'אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3': f'{BASE_URL}/אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3'
 }
