d6730181292957707f22a1937de10ef7006b7696
noamtamir <noam.tamir@gmail.com>
2023-09-12 23:58:02 +0200

    load static files

--- player/models.py
+++ player/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from dataclasses import dataclass
 
-BASE_URL = 'http://localhost:3000/static'
 TRACK_LINKS = {
-  'טל אנגרט  -   מאפייני הקיוםBeLateral .mp3': f'{BASE_URL}/טל אנגרט  -   מאפייני הקיוםBeLateral .mp3',
-  'טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3': f'{BASE_URL}/טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3',
-  'אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3': f'{BASE_URL}/אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3'
+  'טל אנגרט  -   מאפייני הקיוםBeLateral .mp3': 'טל אנגרט  -   מאפייני הקיוםBeLateral .mp3',
+  'טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3': 'טל אנגרט  -  פעולה אנושית משותפת  BeLateral.mp3',
+  'אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3': 'אסף פדרמן - 15 דק סריקת גוף BeLateral.mp3'
 }
